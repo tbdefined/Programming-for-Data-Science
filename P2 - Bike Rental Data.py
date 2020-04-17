d224c834afc17f0f3363e8342f27889b1abb973e
tbdefined <brandt_t@gmx.de>
2020-04-17 11:29:07 +0200

    Refactoring of code by deleting unnecessary spaces

--- P2 - Bike Rental Data.py
+++ P2 - Bike Rental Data.py
@@ -7,7 +7,6 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -238,7 +237,6 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
 
 if __name__ == "__main__":
 	main()
